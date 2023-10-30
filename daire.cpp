e6e15950382315e72e9405b09d777e4b37f45a7c
Yusuf <115079732+dalciyusuf@users.noreply.github.com>
2023-10-30 18:18:35 +0300

    Add files via upload

---
+++ daire.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define pi 3.14
+
+main()
+{
+	float r,cevre,alan;
+	
+	printf("dairenin yaricapini giriniz:	");
+	scanf("%f",&r);
+	
+	cevre= 2*pi*r;
+	alan = pi*r*r;
+	
+	printf("cevre=%f alan=%f\n",cevre,alan);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0 ;
+}
